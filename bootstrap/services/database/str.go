b625c31dc68a496047d5cc349d6f2d22c4ef45c6
baichou <lvluo200711@gmail.com>
2022-06-17 18:08:12 +0800

    添加指针函数

---
+++ bootstrap/services/database/str.go
@@ -0,0 +1,25 @@
+package database
+
+func StrPointer(str string) *string {
+	return &str
+}
+
+func IntPointer(i int) *int {
+	return &i
+}
+
+func Int64Pointer(i int64) *int64 {
+	return &i
+}
+
+func UInt64Pointer(i uint64) *uint64 {
+	return &i
+}
+
+func UInt32Pointer(i uint32) *uint32 {
+	return &i
+}
+
+func Int32Pointer(i int32) *int32 {
+	return &i
+}
