268983b5c7ed1c6e17ab8a129bd65c65337ab4ea
baichou <lvluo200711@gmail.com>
2022-06-20 18:27:30 +0800

    类型快速转换函数

--- bootstrap/services/database/str.go
+++ bootstrap/services/database/str.go
@@ -1,25 +1,91 @@
 package database
 
-func StrPointer(str string) *string {
-	return &str
+func StrPointer(str interface{}) *string {
+	switch str.(type) {
+	case string:
+		s := str.(string)
+		return &s
+	case *string:
+		if str == nil {
+			return nil
+		}
+		s := str.(*string)
+		return s
+	}
+	return nil
 }
 
-func IntPointer(i int) *int {
-	return &i
+func IntPointer(i interface{}) *int {
+	switch i.(type) {
+	case int:
+		s := i.(int)
+		return &s
+	case *int:
+		if i == nil {
+			return nil
+		}
+		s := i.(*int)
+		return s
+	}
+	return nil
 }
 
-func Int64Pointer(i int64) *int64 {
-	return &i
+func Int64Pointer(i interface{}) *int64 {
+	switch i.(type) {
+	case int64:
+		s := i.(int64)
+		return &s
+	case *int64:
+		if i == nil {
+			return nil
+		}
+		s := i.(*int64)
+		return s
+	}
+	return nil
 }
 
-func UInt64Pointer(i uint64) *uint64 {
-	return &i
+func UInt64Pointer(i interface{}) *uint64 {
+	switch i.(type) {
+	case uint64:
+		s := i.(uint64)
+		return &s
+	case *uint64:
+		if i == nil {
+			return nil
+		}
+		s := i.(*uint64)
+		return s
+	}
+	return nil
 }
 
-func UInt32Pointer(i uint32) *uint32 {
-	return &i
+func UInt32Pointer(i interface{}) *uint32 {
+	switch i.(type) {
+	case uint32:
+		s := i.(uint32)
+		return &s
+	case *uint32:
+		if i == nil {
+			return nil
+		}
+		s := i.(*uint32)
+		return s
+	}
+	return nil
 }
 
-func Int32Pointer(i int32) *int32 {
-	return &i
+func Int32Pointer(i interface{}) *int32 {
+	switch i.(type) {
+	case int32:
+		s := i.(int32)
+		return &s
+	case *int32:
+		if i == nil {
+			return nil
+		}
+		s := i.(*int32)
+		return s
+	}
+	return nil
 }
