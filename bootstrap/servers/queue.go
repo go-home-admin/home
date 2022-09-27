cff4127277ac13767d57fd10e0158bb96f1ba680
aliang <zodial@qq.com>
2022-09-27 15:29:29 +0800

    fix: queue

--- bootstrap/servers/queue.go
+++ bootstrap/servers/queue.go
@@ -224,11 +224,7 @@ func (q *Queue) runBaseQueue(group string, streams []string) {
 			time.Sleep(3 * time.Second)
 		} else if strings.Contains(cmd.Err().Error(), "NOGROUP") {
 			// 初始化Stream和Group
-			for _, stream := range streams {
-				if stream != ">" {
-					q.Connect.Client.XGroupCreateMkStream(ctx, stream, group, "0")
-				}
-			}
+			q.Connect.Client.XGroupCreateMkStream(ctx, streams[0], group, "0")
 			time.Sleep(3 * time.Second)
 		} else {
 			log.Errorf("redis命令未知错误, XReadGroup, %v", cmd.Err().Error())
@@ -315,6 +311,10 @@ func (q *Queue) runSerialQueue(group string, streams []string) {
 				}
 			}
 		} else if cmd.Err().Error() == "redis: nil" {
+			time.Sleep(3 * time.Second)
+		} else if strings.Contains(cmd.Err().Error(), "NOGROUP") {
+			// 初始化Stream和Group
+			q.Connect.Client.XGroupCreateMkStream(ctx, streams[0], group, "0")
 			time.Sleep(3 * time.Second)
 		} else {
 			log.Error(cmd.Err().Error())
