d646ff209559d754750ebbeea29da9d9e0d6e65f
aliang <zodial@qq.com>
2022-10-24 18:49:09 +0800

    feat: add func "StringToHump"

--- app/func.go
+++ app/func.go
@@ -66,3 +66,28 @@ func GetGoId() uint64 {
 	n, _ := strconv.ParseUint(string(b), 10, 64)
 	return n
 }
+
+// StringToHump 蛇形转驼峰
+func StringToHump(s string) string {
+	data := make([]byte, 0, len(s))
+	j := false
+	k := false
+	num := len(s) - 1
+	for i := 0; i <= num; i++ {
+		d := s[i]
+		if k == false && d >= 'A' && d <= 'Z' {
+			k = true
+		}
+		if d >= 'a' && d <= 'z' && (j || k == false) {
+			d = d - 32
+			j = false
+			k = true
+		}
+		if k && (d == '_' || d == '-') && num > i && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			j = true
+			continue
+		}
+		data = append(data, d)
+	}
+	return string(data[:])
+}
