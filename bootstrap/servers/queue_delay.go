c7aec67690a2202a07d8c7cf5d636e9f460d0b27
yuanzhao <2206582181@qq.com>
2022-12-14 10:07:07 +0800

    fix: 延时队列表修改

--- bootstrap/servers/queue_delay.go
+++ bootstrap/servers/queue_delay.go
@@ -19,14 +19,27 @@ type DelayQueue interface {
 	Run()
 }
 
+var createTable = `
+CREATE TABLE ` + "`" + `delay_queue` + "`" + ` (
+  ` + "`" + `id` + "`" + ` varchar(64) NOT NULL,
+  ` + "`" + `fail` + "`" + ` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '失败次数',
+  ` + "`" + `route` + "`" + ` varchar(254) NOT NULL COMMENT '路由',
+  ` + "`" + `job` + "`" + ` json NOT NULL COMMENT '任务信息',
+  ` + "`" + `run_at` + "`" + ` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '执行时间点',
+  ` + "`" + `created_at` + "`" + ` timestamp NOT NULL DEFAULT '2022-08-25 00:00:00' COMMENT '创建时间',
+  PRIMARY KEY (` + "`" + `id` + "`" + `),
+  KEY ` + "`" + `idx_delay_queue_run_at` + "`" + ` (` + "`" + `run_at` + "`" + `)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='延时队列存储';
+`
+
 // OrmDelayQueue @Bean
 type OrmDelayQueue struct {
-	Id        string        `gorm:"primaryKey;autoIncrement;column:id;type:varchar(64);not null" json:"id"`
-	Fail      int           `gorm:"column:fail;type:int" json:"fail"`
-	Route     string        `gorm:"column:route;type:varchar(254)" json:"route"`
-	Job       database.JSON `gorm:"column:job;type:json" json:"job"`
-	RunAt     database.Time `gorm:"column:run_at;type:timestamp;index;not null" json:"run_at"`
-	CreatedAt database.Time `gorm:"column:created_at;type:timestamp;not null;default:'2022-08-25 00:00:00'" json:"created_at"`
+	Id        string        `gorm:"column:id;type:varchar(64);primaryKey"`                                                                           //
+	Fail      uint64        `gorm:"column:fail;type:bigint(20) unsigned;default:0;comment:'失败次数'"`                                                   // 失败次数
+	Route     string        `gorm:"column:route;type:varchar(254);comment:'路由'"`                                                                     // 路由
+	Job       database.JSON `gorm:"column:job;type:json;comment:'任务信息'"`                                                                             // 任务信息
+	RunAt     database.Time `gorm:"column:run_at;type:timestamp;index:idx_delay_queue_run_at,class:BTREE;default:CURRENT_TIMESTAMP;comment:'执行时间点'"` // 执行时间点
+	CreatedAt database.Time `gorm:"column:created_at;type:timestamp;default:2022-08-25 00:00:00;comment:'创建时间'"`                                     // 创建时间
 }
 
 func (receiver *OrmDelayQueue) TableName() string {
@@ -42,7 +55,7 @@ type DelayQueueForMysql struct {
 func (d *DelayQueueForMysql) Init() {
 	if app2.Config("queue.delay.auth_migrate", true) {
 		logrus.Info("你可以修改queue.delay.auth_migrate = false; 关闭自动迁移delay_queue表")
-		err := d.mysql.AutoMigrate(&OrmDelayQueue{})
+		err := d.mysql.Exec(createTable)
 		if err != nil {
 			panic(err)
 			return
@@ -83,7 +96,8 @@ func (d *DelayQueueForMysql) Loop() {
 				v := job.Interface()
 				newJob, ok := v.(constraint.Job)
 				if ok {
-					err := json.Unmarshal([]byte(event), newJob)
+					by, _ := event.MarshalJSON()
+					err := json.Unmarshal(by, newJob)
 					if err == nil {
 						newJob.Handler()
 						delIds = append(delIds, delayMsg.Id)
